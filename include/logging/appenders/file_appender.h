6d425f6697da79d2af38cf14edb6cdae150152e7
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-08 00:23:53 +0300

    Add file appender

---
+++ include/logging/appenders/file_appender.h
@@ -0,0 +1,65 @@
+/*!
+    \file file_appender.h
+    \brief File appender definition
+    \author Ivan Shynkarenka
+    \date 07.09.2016
+    \copyright MIT License
+*/
+
+#ifndef CPPLOGGING_APPENDERS_FILE_APPENDER_H
+#define CPPLOGGING_APPENDERS_FILE_APPENDER_H
+
+#include "logging/appender.h"
+
+#include "filesystem/file.h"
+
+namespace CppLogging {
+
+//! File appender
+/*!
+    File appender writes the given logging record into the file with
+    the given file name. In case of any IO error this appender will
+    lost the logging record, but try to recover from fail in a short
+    interval of 100ms.
+
+    Not thread-safe.
+*/
+class FileAppender : public Appender
+{
+public:
+    //! Initialize the appender with a given file, truncate/append and auto-flush flags
+    /*!
+         \param file - File
+         \param truncate - Truncate flag (default is false)
+         \param auto_flush - Auto-flush flag (default is false)
+    */
+    explicit FileAppender(const CppCommon::File& file, bool truncate = false, bool auto_flush = false);
+    FileAppender(const FileAppender&) = delete;
+    FileAppender(FileAppender&&) = default;
+    ~FileAppender() { Flush(); }
+
+    FileAppender& operator=(const FileAppender&) = delete;
+    FileAppender& operator=(FileAppender&&) = default;
+
+    // Implementation of Appender
+    void AppendRecord(Record& record) override;
+    void Flush() override;
+
+private:
+    CppCommon::Timestamp _retry;
+    CppCommon::File _file;
+    bool _truncate;
+    bool _auto_flush;
+
+    //! Check if the file is opened for writing
+    /*
+        - If the file is opened and ready to write immediately returns true
+        - If the last retry was earlier than 100ms immediately returns false
+        - If the file is closed try to open it for writing, returns true/false
+    */
+    bool CheckFileIsOpened();
+};
+
+} // namespace CppLogging
+
+#endif // CPPLOGGING_APPENDERS_FILE_APPENDER_H
---
+++ source/logging/appenders/file_appender.cpp
@@ -0,0 +1,92 @@
+/*!
+    \file file_appender.cpp
+    \brief File appender implementation
+    \author Ivan Shynkarenka
+    \date 07.09.2016
+    \copyright MIT License
+*/
+
+#include "logging/appenders/file_appender.h"
+
+namespace CppLogging {
+
+FileAppender::FileAppender(const CppCommon::File& file, bool truncate, bool auto_flush)
+    : _retry(0), _file(file), _truncate(truncate), _auto_flush(auto_flush)
+{
+    CheckFileIsOpened();
+}
+
+void FileAppender::AppendRecord(Record& record)
+{
+    // Skip logging records without layout
+    if (record.raw.empty())
+        return;
+
+    if (CheckFileIsOpened())
+    {
+        // Try to write logging record content into the opened file
+        try
+        {
+            _file.Write(record.raw.data(), record.raw.size());
+        }
+        catch (CppCommon::FileSystemException&)
+        {
+            // Try to close the opened file in case of any IO error
+            try
+            {
+                _file.Close();
+            }
+            catch (CppCommon::FileSystemException&) {}
+        }
+    }
+}
+
+void FileAppender::Flush()
+{
+    if (CheckFileIsOpened())
+    {
+        // Try to flush the opened file
+        try
+        {
+            _file.Flush();
+        }
+        catch (CppCommon::FileSystemException&)
+        {
+            // Try to close the opened file in case of any IO error
+            try
+            {
+                _file.Close();
+            }
+            catch (CppCommon::FileSystemException&) {}
+        }
+    }
+}
+
+bool FileAppender::CheckFileIsOpened()
+{
+    try
+    {
+        // 1. Check if the file is already opened for writing
+        if (_file.IsFileWriteOpened())
+            return true;
+        // 2. Check retry timestamp if 100ms elapsed after the last attempt
+        if ((CppCommon::Timestamp::nano() - _retry).milliseconds() < 100)
+            return false;
+        // 3. If the file is opened for reading close it
+        if (_file.IsFileReadOpened())
+            _file.Close();
+        // 4. Open the file for writing
+        _file.OpenOrCreate(false, true, _truncate);
+        // 5. Reset the the retry timestamp
+        _retry = CppCommon::Timestamp::nano();
+        return true;
+    }
+    catch (CppCommon::FileSystemException&)
+    {
+        // In case of any IO error reset the retry timestamp and return false!
+        _retry = CppCommon::Timestamp::nano();
+        return false;
+    }
+}
+
+} // namespace CppLogging
