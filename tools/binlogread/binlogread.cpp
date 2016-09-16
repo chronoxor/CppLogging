428fc579dca4e7f9076b16abd5d147ab941366f2
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-16 15:25:27 +0300

    Add binlogread tool

---
+++ tools/binlogread/binlogread.cpp
@@ -0,0 +1,52 @@
+/*!
+    \file filesystem_file.cpp
+    \brief Filesystem file wrapper example
+    \author Ivan Shynkarenka
+    \date 04.09.2016
+    \copyright MIT License
+*/
+
+#include "filesystem/file.h"
+#include "utility/countof.h"
+
+#include <iostream>
+
+#include <OptionParser.h>
+
+int main(int argc, char** argv)
+{
+    auto parser = optparse::OptionParser().version(version);
+
+    parser.add_option("-h", "--help").help("Show help");
+    parser.add_option("-i", "--input").help("Input file name");
+    parser.add_option("-o", "--output").help("Output file name");
+
+    optparse::Values options = parser.parse_args(argc, argv);
+
+    char buffer[] = "The quick brown fox jumps over the lazy dog";
+
+    // Create file for writing
+    CppCommon::File file("example.txt");
+    file.Create(false, true);
+
+    // Write buffer into the file
+    file.Write((uint8_t*)buffer, CppCommon::countof(buffer) - 1);
+
+    // Flush the file
+    file.Flush();
+
+    // Close the file
+    file.Close();
+
+    std::cout << "File size: " << file.size() << std::endl;
+
+    // Read all text from the file
+    std::string text = CppCommon::File::ReadAllText(file);
+
+    std::cout << "File content: " << text << std::endl;
+
+    // Remove file
+    CppCommon::File::Remove(file);
+
+    return 0;
+}
