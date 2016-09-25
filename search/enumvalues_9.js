d6e48a06da176895fe9cca85d6f2f93ebd8ed7ef
AppVeyor <team@appveyor.com>
2016-09-25 23:17:45 +0000

    Update generated documentation

--- search/enumvalues_9.js
+++ search/enumvalues_9.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['second',['SECOND',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a2200becb80f0019c4a2ccecec350d0db',1,'CppLogging']]],
-  ['string',['String',['../class_cpp_logging_1_1_time_policy_impl.html#aa30ee22b41529294624aa57ca9894a71a27118326006d3829667a400ad23d5d98',1,'CppLogging::TimePolicyImpl::String()'],['../class_cpp_logging_1_1_text_layout_1_1_impl.html#a25dcbdead45ae15f29ab124b9e29e748a27118326006d3829667a400ad23d5d98',1,'CppLogging::TextLayout::Impl::String()']]]
+  ['warn',['WARN',['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba32bd8a1db2275458673903bdb84cb277',1,'CppLogging']]]
 ];
