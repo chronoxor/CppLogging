6faae4e7ac93cc687e3b51d25968099ced1213f8
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-30 02:29:09 +0300

    build

--- modules/CppCommon.cmake
+++ modules/CppCommon.cmake
@@ -1,5 +1,12 @@
 if(NOT TARGET cppcommon)
+
+  # Set module flag
   set(CPPCOMMON_MODULE Y)
+
+  # Add module subdirectory
   add_subdirectory("CppCommon")
+
+  # Set module folder
   set_target_properties(cppcommon PROPERTIES FOLDER modules/CppCommon)
+
 endif()
--- modules/zlib.cmake
+++ modules/zlib.cmake
@@ -1,16 +1,31 @@
 if(NOT TARGET zlib)
+
+  # Temporary disable some warnings
+  # C4131: 'function' : uses old-style declarator
+  # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
   if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
     set(CMAKE_OLD_C_FLAGS ${CMAKE_C_FLAGS})
     set(CMAKE_OLD_CXX_FLAGS ${CMAKE_CXX_FLAGS})
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4127 /wd4131 /wd4244")
-    set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} /wd4127 /wd4131 /wd4244")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4127 /wd4131 /wd4244")
   endif()
+
+  # Set module options
   option(AMD64 "Enable building amd64 assembly implementation" ON)
+
+  # Add module subdirectory
   add_subdirectory("zlib")
+
+  # Set module folder
   set_target_properties(zlib zlibstatic example minigzip PROPERTIES FOLDER modules/zlib)
+
+  # Exclude some modules from the build
   set_target_properties(example minigzip PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+
+  # Restore default warnings
   if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
     set(CMAKE_C_FLAGS ${CMAKE_OLD_C_FLAGS})
     set(CMAKE_CXX_FLAGS ${CMAKE_OLD_CXX_FLAGS})
   endif()
+
 endif()
