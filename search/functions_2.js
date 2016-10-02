2ea8afc3a394d50d215ae78b4b7811eef7850dca
Travis CI <travis@travis-ci.org>
2016-10-02 02:24:41 +0000

    Update generated documentation

--- search/functions_2.js
+++ search/functions_2.js
@@ -3,7 +3,7 @@ var searchData=
   ['call_5fzopen64',['call_zopen64',['../ioapi_8c.html#aba1d7f2b69248eee96210089ce140925',1,'ioapi.c']]],
   ['call_5fzseek64',['call_zseek64',['../ioapi_8c.html#aa3566299e354e3275a72193ab48e4af3',1,'ioapi.c']]],
   ['call_5fztell64',['call_ztell64',['../ioapi_8c.html#ad18411c799d4ef86d37e25610e411c93',1,'ioapi.c']]],
-  ['capacity',['capacity',['../class_cpp_logging_1_1_async_buffer.html#a0de99ebced85547882049a526025b981',1,'CppLogging::AsyncBuffer']]],
+  ['capacity',['capacity',['../class_cpp_logging_1_1_async_buffer.html#ab4a7d19933ed60cdf4a07e4732e5f966',1,'CppLogging::AsyncBuffer']]],
   ['clear',['Clear',['../class_cpp_logging_1_1_record.html#a329f4aee55d7a67be3f313449b4bdf21',1,'CppLogging::Record']]],
   ['config',['Config',['../class_cpp_logging_1_1_config.html#a36b0110d0dab4d9ff1a273ee527b03d8',1,'CppLogging::Config::Config(const Config &amp;)=delete'],['../class_cpp_logging_1_1_config.html#a33aed3ee45a629bf472d9ab5f4c2a0c5',1,'CppLogging::Config::Config(Config &amp;&amp;)=delete']]],
   ['configlogger',['ConfigLogger',['../class_cpp_logging_1_1_config.html#aa3e1df3aa623bada0b5523294e9fde14',1,'CppLogging::Config::ConfigLogger(const std::shared_ptr&lt; Processor &gt; &amp;sink)'],['../class_cpp_logging_1_1_config.html#a8c0cea746d092d4e48ed10796fa7cea1',1,'CppLogging::Config::ConfigLogger(const std::string &amp;name, const std::shared_ptr&lt; Processor &gt; &amp;sink)']]],
