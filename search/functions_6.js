ce2e025f33ba7e9ec8eb11ca20243dc357e4675b
Travis CI <travis@travis-ci.org>
2016-12-30 15:55:38 +0000

    Update generated documentation

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['info',['Info',['../class_cpp_logging_1_1_logger.html#ad8e3ccfb7e51713e6f4d70ea4c5e90c6',1,'CppLogging::Logger']]],
+  ['info',['Info',['../class_cpp_logging_1_1_logger.html#af6f31768566da948077112c91c4bc121',1,'CppLogging::Logger']]],
   ['isenabled',['IsEnabled',['../class_cpp_logging_1_1_trigger.html#a3ec7ca4993b7ee110fccc5984caa7901',1,'CppLogging::Trigger']]]
 ];
