7ddda432b741d46c91fc75c82d693522d6b4a3df
AppVeyor <team@appveyor.com>
2017-02-27 03:27:14 +0000

    Update generated documentation

--- search/all_e.js
+++ search/all_e.js
@@ -1,13 +1,15 @@
 var searchData=
 [
   ['second',['SECOND',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a2200becb80f0019c4a2ccecec350d0db',1,'CppLogging']]],
-  ['size',['size',['../class_cpp_logging_1_1_async_buffer.html#af69c09a3e48091d02f726e13b29443d4',1,'CppLogging::AsyncBuffer']]],
+  ['size',['size',['../class_cpp_logging_1_1_async_buffer.html#a9a3bd7bec908877f6c1465532df749d9',1,'CppLogging::AsyncBuffer']]],
   ['sizepolicyimpl',['SizePolicyImpl',['../class_cpp_logging_1_1_rolling_file_appender.html#a0d453fa68a659898193acb608bf94cda',1,'CppLogging::RollingFileAppender']]],
   ['swap',['swap',['../class_cpp_logging_1_1_record.html#a05103353f6464abaed30526a45b6ad39',1,'CppLogging::Record::swap(Record &amp;record) noexcept'],['../class_cpp_logging_1_1_record.html#a41307f7cef7933943d41aa2f13fcef01',1,'CppLogging::Record::swap()'],['../namespace_cpp_logging.html#a483acf995015b6623510acb20a4017d8',1,'CppLogging::swap()']]],
   ['sync_5fprocessor_2ecpp',['sync_processor.cpp',['../sync__processor_8cpp.html',1,'']]],
   ['sync_5fprocessor_2eh',['sync_processor.h',['../sync__processor_8h.html',1,'']]],
-  ['syncprocessor',['SyncProcessor',['../class_cpp_logging_1_1_sync_processor.html',1,'CppLogging::SyncProcessor'],['../class_cpp_logging_1_1_sync_processor.html#a986d3bea55cccc010878264050ee51cd',1,'CppLogging::SyncProcessor::SyncProcessor()=default'],['../class_cpp_logging_1_1_sync_processor.html#a85d47bca89b0d9cc298e8a2b8c8a86fc',1,'CppLogging::SyncProcessor::SyncProcessor(const SyncProcessor &amp;)=delete'],['../class_cpp_logging_1_1_sync_processor.html#a104d11d8bba6097ae50e5e4122e0ca94',1,'CppLogging::SyncProcessor::SyncProcessor(SyncProcessor &amp;&amp;)=default']]],
+  ['syncprocessor',['SyncProcessor',['../class_cpp_logging_1_1_sync_processor.html',1,'CppLogging']]],
+  ['syncprocessor',['SyncProcessor',['../class_cpp_logging_1_1_sync_processor.html#a986d3bea55cccc010878264050ee51cd',1,'CppLogging::SyncProcessor::SyncProcessor()=default'],['../class_cpp_logging_1_1_sync_processor.html#a85d47bca89b0d9cc298e8a2b8c8a86fc',1,'CppLogging::SyncProcessor::SyncProcessor(const SyncProcessor &amp;)=delete'],['../class_cpp_logging_1_1_sync_processor.html#a104d11d8bba6097ae50e5e4122e0ca94',1,'CppLogging::SyncProcessor::SyncProcessor(SyncProcessor &amp;&amp;)=default']]],
   ['syslog_5fappender_2ecpp',['syslog_appender.cpp',['../syslog__appender_8cpp.html',1,'']]],
   ['syslog_5fappender_2eh',['syslog_appender.h',['../syslog__appender_8h.html',1,'']]],
-  ['syslogappender',['SyslogAppender',['../class_cpp_logging_1_1_syslog_appender.html',1,'CppLogging::SyslogAppender'],['../class_cpp_logging_1_1_syslog_appender.html#acbe088ad90296c1fc16e51273782dd87',1,'CppLogging::SyslogAppender::SyslogAppender()'],['../class_cpp_logging_1_1_syslog_appender.html#a2d57bf4dee6a385731aa3528f3832b96',1,'CppLogging::SyslogAppender::SyslogAppender(const SyslogAppender &amp;)=delete'],['../class_cpp_logging_1_1_syslog_appender.html#a9c49d5c12c5c5952bd334fc7c44f101a',1,'CppLogging::SyslogAppender::SyslogAppender(SyslogAppender &amp;&amp;)=default']]]
+  ['syslogappender',['SyslogAppender',['../class_cpp_logging_1_1_syslog_appender.html',1,'CppLogging']]],
+  ['syslogappender',['SyslogAppender',['../class_cpp_logging_1_1_syslog_appender.html#acbe088ad90296c1fc16e51273782dd87',1,'CppLogging::SyslogAppender::SyslogAppender()'],['../class_cpp_logging_1_1_syslog_appender.html#a2d57bf4dee6a385731aa3528f3832b96',1,'CppLogging::SyslogAppender::SyslogAppender(const SyslogAppender &amp;)=delete'],['../class_cpp_logging_1_1_syslog_appender.html#a9c49d5c12c5c5952bd334fc7c44f101a',1,'CppLogging::SyslogAppender::SyslogAppender(SyslogAppender &amp;&amp;)=default']]]
 ];
