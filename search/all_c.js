ed14b06f82b37d25289bddeecc3891e5ed60a562
Travis CI <travis@travis-ci.org>
2017-02-27 02:14:38 +0000

    Update generated documentation

--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['pattern',['pattern',['../class_cpp_logging_1_1_text_layout.html#a6fb8421162a8bfb0d740da665477e746',1,'CppLogging::TextLayout']]],
+  ['pattern',['pattern',['../class_cpp_logging_1_1_text_layout.html#ab3e9c2d158ac22b7f9e18a1985998fd1',1,'CppLogging::TextLayout']]],
   ['processor',['Processor',['../class_cpp_logging_1_1_processor.html',1,'CppLogging']]],
   ['processor',['Processor',['../class_cpp_logging_1_1_processor.html#ab9e8bf067a254854f6f5bb8a108c1105',1,'CppLogging::Processor::Processor()=default'],['../class_cpp_logging_1_1_processor.html#a18d2e051692b2ef3096ae5cca6a3ee58',1,'CppLogging::Processor::Processor(const Processor &amp;)=default'],['../class_cpp_logging_1_1_processor.html#aa9dbbc495f038392b4ff63d635edb657',1,'CppLogging::Processor::Processor(Processor &amp;&amp;)=default']]],
   ['processor_2ecpp',['processor.cpp',['../processor_8cpp.html',1,'']]],
