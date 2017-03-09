a5cc34ce7a929b97c4dac71836dd905fd0ca9a2e
Travis CI <travis@travis-ci.org>
2017-03-09 00:58:15 +0000

    Update generated documentation

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['enable',['Enable',['../class_cpp_logging_1_1_trigger.html#a2df9ab0b929772cd7b4b5a990102b1d5',1,'CppLogging::Trigger']]],
   ['enqueue',['Enqueue',['../class_cpp_logging_1_1_async_buffer.html#a9db4993d859111d09a1b1bc23b3a787d',1,'CppLogging::AsyncBuffer']]],
-  ['error',['Error',['../class_cpp_logging_1_1_logger.html#ac6a9f7a2fbf3d8024a24b3c0bd5d7f25',1,'CppLogging::Logger']]],
+  ['error',['Error',['../class_cpp_logging_1_1_logger.html#a29beea93f046e04348f2a47dcd3e3603',1,'CppLogging::Logger']]],
   ['errorappender',['ErrorAppender',['../class_cpp_logging_1_1_error_appender.html#abe2c0effcb87d346003d28756fa7a43e',1,'CppLogging::ErrorAppender::ErrorAppender()=default'],['../class_cpp_logging_1_1_error_appender.html#a6687f19113878c2f39cf18a5673982e8',1,'CppLogging::ErrorAppender::ErrorAppender(const ErrorAppender &amp;)=delete'],['../class_cpp_logging_1_1_error_appender.html#a45986a8b03bfb9be12501b04b0aeeb60',1,'CppLogging::ErrorAppender::ErrorAppender(ErrorAppender &amp;&amp;)=default']]]
 ];
