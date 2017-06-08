12118bf6a9a4ad2e59c919a7e73a37d9078cad4d
Travis CI <travis@travis-ci.org>
2017-06-08 20:38:02 +0000

    Update generated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#ab618fb4b454dadd7602bd00719216e53',1,'CppLogging::Logger']]],
+  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#adaaba9a3da23927ac4ee7630ee29d11c',1,'CppLogging::Logger']]],
   ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html#a904ca30b987c25394616706f2f61093f',1,'CppLogging::DebugAppender::DebugAppender()=default'],['../class_cpp_logging_1_1_debug_appender.html#aa86c3ffb7964bacfafbc7590048c9848',1,'CppLogging::DebugAppender::DebugAppender(const DebugAppender &amp;)=delete'],['../class_cpp_logging_1_1_debug_appender.html#ad86fc459a31d7fd0405852529e6f38e3',1,'CppLogging::DebugAppender::DebugAppender(DebugAppender &amp;&amp;)=default']]],
   ['dequeue',['Dequeue',['../class_cpp_logging_1_1_async_buffer.html#a83acbdf7abb012e6379f4001b1a4d8ee',1,'CppLogging::AsyncBuffer']]],
   ['disable',['Disable',['../class_cpp_logging_1_1_trigger.html#afa07373f15ec3ecd7618d391059d4b07',1,'CppLogging::Trigger']]]
