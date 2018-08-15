58efda46dd2cba483051446bcbd647bad81f2ebc
Travis CI <travis@travis-ci.org>
2018-08-15 02:05:51 +0000

    Update generated documentation

--- search/all_a.js
+++ search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['none',['NONE',['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3bab50339a10e1de285ac99d4c3990b8693',1,'CppLogging']]],
   ['null_5fappender_2eh',['null_appender.h',['../null__appender_8h.html',1,'']]],
-  ['nullappender',['NullAppender',['../class_cpp_logging_1_1_null_appender.html',1,'CppLogging::NullAppender'],['../class_cpp_logging_1_1_null_appender.html#af2d9dedec941a77bbc593f9d33ee50de',1,'CppLogging::NullAppender::NullAppender()=default'],['../class_cpp_logging_1_1_null_appender.html#a5c81fcbc737e59f7cf57c7acb94d49b8',1,'CppLogging::NullAppender::NullAppender(const NullAppender &amp;)=delete'],['../class_cpp_logging_1_1_null_appender.html#a23ad444508e6042db5bd8015aa12d719',1,'CppLogging::NullAppender::NullAppender(NullAppender &amp;&amp;) noexcept=default']]]
+  ['nullappender',['NullAppender',['../class_cpp_logging_1_1_null_appender.html#af2d9dedec941a77bbc593f9d33ee50de',1,'CppLogging::NullAppender::NullAppender()=default'],['../class_cpp_logging_1_1_null_appender.html#a5c81fcbc737e59f7cf57c7acb94d49b8',1,'CppLogging::NullAppender::NullAppender(const NullAppender &amp;)=delete'],['../class_cpp_logging_1_1_null_appender.html#a23ad444508e6042db5bd8015aa12d719',1,'CppLogging::NullAppender::NullAppender(NullAppender &amp;&amp;) noexcept=default']]],
+  ['nullappender',['NullAppender',['../class_cpp_logging_1_1_null_appender.html',1,'CppLogging']]]
 ];
