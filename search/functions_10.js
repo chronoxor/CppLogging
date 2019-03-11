8ff57e95a040c64b7ad545094e121be8e3117247
Travis CI <travis@travis-ci.org>
2019-03-11 23:07:44 +0000

    Update generated documentation

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['warn',['Warn',['../class_cpp_logging_1_1_logger.html#aad8e03ba204d8ecaf55d71035d754921',1,'CppLogging::Logger::Warn(const std::string &amp;warn)'],['../class_cpp_logging_1_1_logger.html#a65f1b207d94ef1afdb7853278b28c8a2',1,'CppLogging::Logger::Warn(const char *warn, const Args &amp;... args)']]]
+  ['warn',['Warn',['../class_cpp_logging_1_1_logger.html#aad8e03ba204d8ecaf55d71035d754921',1,'CppLogging::Logger::Warn(const std::string &amp;warn)'],['../class_cpp_logging_1_1_logger.html#ab0a428aff1b55f33317c95549bc4ef4a',1,'CppLogging::Logger::Warn(const char *warn, const Args &amp;...args)']]]
 ];
