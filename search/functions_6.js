e2ab1759f38017219d85b8b63e422d58ddc09d41
Travis CI <travis@travis-ci.org>
2019-03-11 12:20:42 +0000

    Update generated documentation

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a47202ec8d0f24ab9d459f7866b121143',1,'CppLogging::Logger::Info(const std::string &amp;info)'],['../class_cpp_logging_1_1_logger.html#af6f31768566da948077112c91c4bc121',1,'CppLogging::Logger::Info(const char *info, const Args &amp;...args)']]],
+  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a47202ec8d0f24ab9d459f7866b121143',1,'CppLogging::Logger::Info(const std::string &amp;info)'],['../class_cpp_logging_1_1_logger.html#ad8e3ccfb7e51713e6f4d70ea4c5e90c6',1,'CppLogging::Logger::Info(const char *info, const Args &amp;... args)']]],
   ['isenabled',['IsEnabled',['../class_cpp_logging_1_1_trigger.html#a3ec7ca4993b7ee110fccc5984caa7901',1,'CppLogging::Trigger']]]
 ];
