adcb3ebcde4d9b0ae6093ca2bd056a44f7e471ef
Travis CI <travis@travis-ci.org>
2019-03-11 08:41:02 +0000

    Update generated documentation

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['info',['Info',['../class_cpp_logging_1_1_logger.html#ad8e3ccfb7e51713e6f4d70ea4c5e90c6',1,'CppLogging::Logger::Info()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
+  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a47202ec8d0f24ab9d459f7866b121143',1,'CppLogging::Logger::Info(const std::string &amp;info)'],['../class_cpp_logging_1_1_logger.html#af6f31768566da948077112c91c4bc121',1,'CppLogging::Logger::Info(const char *info, const Args &amp;...args)'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
   ['isenabled',['IsEnabled',['../class_cpp_logging_1_1_trigger.html#a3ec7ca4993b7ee110fccc5984caa7901',1,'CppLogging::Trigger']]]
 ];
