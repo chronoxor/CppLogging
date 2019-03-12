a53888b6f82156d85f01aac0ef5f729c77e84e3a
Travis CI <travis@travis-ci.org>
2019-03-12 10:59:41 +0000

    Update generated documentation

--- search/all_f.js
+++ search/all_f.js
@@ -2,15 +2,13 @@ var searchData=
 [
   ['text_5flayout_2ecpp',['text_layout.cpp',['../text__layout_8cpp.html',1,'']]],
   ['text_5flayout_2eh',['text_layout.h',['../text__layout_8h.html',1,'']]],
-  ['textlayout',['TextLayout',['../class_cpp_logging_1_1_text_layout.html',1,'CppLogging']]],
-  ['textlayout',['TextLayout',['../class_cpp_logging_1_1_text_layout.html#ae2a4ca241c2cc54df25a30287255ef22',1,'CppLogging::TextLayout::TextLayout(const std::string &amp;pattern=&quot;{UtcDateTime} [{Thread}] {Level} {Logger} - {Message}{EndLine}&quot;)'],['../class_cpp_logging_1_1_text_layout.html#a681fb4962a1d348b345aca1addf3f305',1,'CppLogging::TextLayout::TextLayout(const TextLayout &amp;)=delete'],['../class_cpp_logging_1_1_text_layout.html#a862320acb8babeac72da86278dc9b2ac',1,'CppLogging::TextLayout::TextLayout(TextLayout &amp;&amp;layout) noexcept']]],
+  ['textlayout',['TextLayout',['../class_cpp_logging_1_1_text_layout.html',1,'CppLogging::TextLayout'],['../class_cpp_logging_1_1_text_layout.html#ae2a4ca241c2cc54df25a30287255ef22',1,'CppLogging::TextLayout::TextLayout(const std::string &amp;pattern=&quot;{UtcDateTime} [{Thread}] {Level} {Logger} - {Message}{EndLine}&quot;)'],['../class_cpp_logging_1_1_text_layout.html#a681fb4962a1d348b345aca1addf3f305',1,'CppLogging::TextLayout::TextLayout(const TextLayout &amp;)=delete'],['../class_cpp_logging_1_1_text_layout.html#a862320acb8babeac72da86278dc9b2ac',1,'CppLogging::TextLayout::TextLayout(TextLayout &amp;&amp;layout) noexcept']]],
   ['thread',['thread',['../class_cpp_logging_1_1_record.html#a816e732d29b4dcd29a7f073b40b20738',1,'CppLogging::Record']]],
   ['timepolicyimpl',['TimePolicyImpl',['../class_cpp_logging_1_1_rolling_file_appender.html#a6d81316f3ffde8a367df1396d02a7d6f',1,'CppLogging::RollingFileAppender']]],
   ['timerollingpolicy',['TimeRollingPolicy',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57',1,'CppLogging']]],
   ['timestamp',['timestamp',['../class_cpp_logging_1_1_record.html#acb824eebe901b423004a65d73dd1ce4d',1,'CppLogging::Record']]],
   ['to',['to',['../class_cpp_logging_1_1_level_filter.html#a49f5195d523f2311507bee63bcbdb861',1,'CppLogging::LevelFilter']]],
   ['toggle',['Toggle',['../class_cpp_logging_1_1_trigger.html#a476aab6d9df45cdf6edd531a8f522628',1,'CppLogging::Trigger']]],
-  ['trigger',['Trigger',['../class_cpp_logging_1_1_trigger.html#add5ecc4976764319b3194165d8b4fc58',1,'CppLogging::Trigger::Trigger(bool state=true)'],['../class_cpp_logging_1_1_trigger.html#a644ce7ddb13f11fb1e5521a4a4a90d8a',1,'CppLogging::Trigger::Trigger(const Trigger &amp;)=delete'],['../class_cpp_logging_1_1_trigger.html#a2539e6a2cf040f16637291727cabd51f',1,'CppLogging::Trigger::Trigger(Trigger &amp;&amp;)=default']]],
-  ['trigger',['Trigger',['../class_cpp_logging_1_1_trigger.html',1,'CppLogging']]],
+  ['trigger',['Trigger',['../class_cpp_logging_1_1_trigger.html',1,'CppLogging::Trigger'],['../class_cpp_logging_1_1_trigger.html#add5ecc4976764319b3194165d8b4fc58',1,'CppLogging::Trigger::Trigger(bool state=true)'],['../class_cpp_logging_1_1_trigger.html#a644ce7ddb13f11fb1e5521a4a4a90d8a',1,'CppLogging::Trigger::Trigger(const Trigger &amp;)=delete'],['../class_cpp_logging_1_1_trigger.html#a2539e6a2cf040f16637291727cabd51f',1,'CppLogging::Trigger::Trigger(Trigger &amp;&amp;)=default']]],
   ['trigger_2eh',['trigger.h',['../trigger_8h.html',1,'']]]
 ];
