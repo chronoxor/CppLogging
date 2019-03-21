34f38599d6fc96f1d9b4006a304cd626e2f54b2c
Travis CI <travis@travis-ci.org>
2019-03-21 11:58:22 +0000

    Update generated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#abfbdbccccd408c9b153a13c905d02cd0',1,'CppLogging::Logger::Debug(const std::string &amp;debug)'],['../class_cpp_logging_1_1_logger.html#adaaba9a3da23927ac4ee7630ee29d11c',1,'CppLogging::Logger::Debug(const char *debug, const Args &amp;...args)']]],
+  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#abfbdbccccd408c9b153a13c905d02cd0',1,'CppLogging::Logger::Debug(const std::string &amp;debug)'],['../class_cpp_logging_1_1_logger.html#ab618fb4b454dadd7602bd00719216e53',1,'CppLogging::Logger::Debug(const char *debug, const Args &amp;... args)']]],
   ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html#a904ca30b987c25394616706f2f61093f',1,'CppLogging::DebugAppender::DebugAppender()=default'],['../class_cpp_logging_1_1_debug_appender.html#aa86c3ffb7964bacfafbc7590048c9848',1,'CppLogging::DebugAppender::DebugAppender(const DebugAppender &amp;)=delete'],['../class_cpp_logging_1_1_debug_appender.html#a30d386a7516fbefdbd5909204f92a5b6',1,'CppLogging::DebugAppender::DebugAppender(DebugAppender &amp;&amp;) noexcept=default']]],
   ['dequeue',['Dequeue',['../class_cpp_logging_1_1_async_wait_free_buffer.html#af8e0d3fe63ee2ad24e315b87aa673966',1,'CppLogging::AsyncWaitFreeBuffer']]],
   ['disable',['Disable',['../class_cpp_logging_1_1_trigger.html#afa07373f15ec3ecd7618d391059d4b07',1,'CppLogging::Trigger']]]
