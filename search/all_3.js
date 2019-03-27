67176d1ee09a403c13f46103daabad87fce6b5ca
Travis CI <travis@travis-ci.org>
2019-03-27 14:31:45 +0000

    Update generated documentation

--- search/all_3.js
+++ search/all_3.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['day',['DAY',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a5b1d59b5451c06afb65ab1bc2713cfb4',1,'CppLogging']]],
-  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#ae98f5e479533c41703269d95babdd46c',1,'CppLogging::Logger::Debug()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3badc30ec20708ef7b0f641ef78b7880a15',1,'CppLogging::DEBUG()']]],
+  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#a629c8eea867e3da62611c10a2fa98f7f',1,'CppLogging::Logger::Debug()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3badc30ec20708ef7b0f641ef78b7880a15',1,'CppLogging::DEBUG()']]],
   ['debug_5fappender_2ecpp',['debug_appender.cpp',['../debug__appender_8cpp.html',1,'']]],
   ['debug_5fappender_2eh',['debug_appender.h',['../debug__appender_8h.html',1,'']]],
-  ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html',1,'CppLogging::DebugAppender'],['../class_cpp_logging_1_1_debug_appender.html#a904ca30b987c25394616706f2f61093f',1,'CppLogging::DebugAppender::DebugAppender()=default'],['../class_cpp_logging_1_1_debug_appender.html#aa86c3ffb7964bacfafbc7590048c9848',1,'CppLogging::DebugAppender::DebugAppender(const DebugAppender &amp;)=delete'],['../class_cpp_logging_1_1_debug_appender.html#a30d386a7516fbefdbd5909204f92a5b6',1,'CppLogging::DebugAppender::DebugAppender(DebugAppender &amp;&amp;) noexcept=default']]],
+  ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html#a904ca30b987c25394616706f2f61093f',1,'CppLogging::DebugAppender::DebugAppender()=default'],['../class_cpp_logging_1_1_debug_appender.html#aa86c3ffb7964bacfafbc7590048c9848',1,'CppLogging::DebugAppender::DebugAppender(const DebugAppender &amp;)=delete'],['../class_cpp_logging_1_1_debug_appender.html#a30d386a7516fbefdbd5909204f92a5b6',1,'CppLogging::DebugAppender::DebugAppender(DebugAppender &amp;&amp;) noexcept=default']]],
+  ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html',1,'CppLogging']]],
   ['dequeue',['Dequeue',['../class_cpp_logging_1_1_async_wait_free_buffer.html#af8e0d3fe63ee2ad24e315b87aa673966',1,'CppLogging::AsyncWaitFreeBuffer']]],
   ['disable',['Disable',['../class_cpp_logging_1_1_trigger.html#afa07373f15ec3ecd7618d391059d4b07',1,'CppLogging::Trigger']]]
 ];
