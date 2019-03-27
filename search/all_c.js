f0981ff8bee4824312a7068198ebb77ad9ba1d16
Travis CI <travis@travis-ci.org>
2019-03-27 12:11:00 +0000

    Update generated documentation

--- search/all_c.js
+++ search/all_c.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['pattern',['pattern',['../class_cpp_logging_1_1_logger_filter.html#accb9537a120ad600e204cc62c9907fc2',1,'CppLogging::LoggerFilter::pattern()'],['../class_cpp_logging_1_1_message_filter.html#a5a03b8ce35795c369333ec3f7924ea86',1,'CppLogging::MessageFilter::pattern()'],['../class_cpp_logging_1_1_text_layout.html#ab3e9c2d158ac22b7f9e18a1985998fd1',1,'CppLogging::TextLayout::pattern()']]],
-  ['processor',['Processor',['../class_cpp_logging_1_1_processor.html',1,'CppLogging::Processor'],['../class_cpp_logging_1_1_processor.html#a2146a25052b050256060dbd231b9a496',1,'CppLogging::Processor::Processor(const std::shared_ptr&lt; Layout &gt; &amp;layout)'],['../class_cpp_logging_1_1_processor.html#a18d2e051692b2ef3096ae5cca6a3ee58',1,'CppLogging::Processor::Processor(const Processor &amp;)=default'],['../class_cpp_logging_1_1_processor.html#af909c31ebb473cad9623ca3b84f8cc91',1,'CppLogging::Processor::Processor(Processor &amp;&amp;) noexcept=default']]],
+  ['processor',['Processor',['../class_cpp_logging_1_1_processor.html',1,'CppLogging']]],
+  ['processor',['Processor',['../class_cpp_logging_1_1_processor.html#a2146a25052b050256060dbd231b9a496',1,'CppLogging::Processor::Processor(const std::shared_ptr&lt; Layout &gt; &amp;layout)'],['../class_cpp_logging_1_1_processor.html#a18d2e051692b2ef3096ae5cca6a3ee58',1,'CppLogging::Processor::Processor(const Processor &amp;)=default'],['../class_cpp_logging_1_1_processor.html#af909c31ebb473cad9623ca3b84f8cc91',1,'CppLogging::Processor::Processor(Processor &amp;&amp;) noexcept=default']]],
   ['processor_2ecpp',['processor.cpp',['../processor_8cpp.html',1,'']]],
   ['processor_2eh',['processor.h',['../processor_8h.html',1,'']]],
   ['processors',['processors',['../class_cpp_logging_1_1_processor.html#ab4d96b6012bee770a07a3f8a25a836a3',1,'CppLogging::Processor']]],
