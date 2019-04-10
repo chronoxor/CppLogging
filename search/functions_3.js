f4f96ca6f910c10b3f293e17ecaeaa997d858a33
Travis CI <travis@travis-ci.org>
2019-04-10 15:46:44 +0000

    Update generated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#ae98f5e479533c41703269d95babdd46c',1,'CppLogging::Logger']]],
+  ['debug',['Debug',['../class_cpp_logging_1_1_logger.html#a629c8eea867e3da62611c10a2fa98f7f',1,'CppLogging::Logger']]],
   ['debugappender',['DebugAppender',['../class_cpp_logging_1_1_debug_appender.html#a904ca30b987c25394616706f2f61093f',1,'CppLogging::DebugAppender::DebugAppender()=default'],['../class_cpp_logging_1_1_debug_appender.html#aa86c3ffb7964bacfafbc7590048c9848',1,'CppLogging::DebugAppender::DebugAppender(const DebugAppender &amp;)=delete'],['../class_cpp_logging_1_1_debug_appender.html#a50bdb07295d55f08c2a50ff0527b4503',1,'CppLogging::DebugAppender::DebugAppender(DebugAppender &amp;&amp;)=delete']]],
   ['dequeue',['Dequeue',['../class_cpp_logging_1_1_async_wait_free_queue.html#ac11860cfc8a710bd7b2fa6129eb0144f',1,'CppLogging::AsyncWaitFreeQueue']]],
   ['disable',['Disable',['../class_cpp_logging_1_1_trigger.html#afa07373f15ec3ecd7618d391059d4b07',1,'CppLogging::Trigger']]]
