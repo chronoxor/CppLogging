d0a6b86f578772765b1ce73b8831af377aa741ca
Travis CI <travis@travis-ci.org>
2019-04-12 21:51:12 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a68863a49a2fb24e5a5760bf44eef387f',1,'CppLogging::Logger::Info()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
+  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a4b893bb6ec96c212ee2fade51a206cc5',1,'CppLogging::Logger::Info()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
   ['isenabled',['IsEnabled',['../class_cpp_logging_1_1_trigger.html#a3ec7ca4993b7ee110fccc5984caa7901',1,'CppLogging::Trigger']]],
   ['isformatstored',['IsFormatStored',['../class_cpp_logging_1_1_record.html#ac698292f7a70db2effa97351eadc21b6',1,'CppLogging::Record']]]
 ];
