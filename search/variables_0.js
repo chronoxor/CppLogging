655302dfbcb3c2ac972edfca7157fe344048ac40
Travis CI <travis@travis-ci.org>
2019-04-21 22:18:35 +0000

    Update generated documentation

--- search/variables_0.js
+++ search/variables_0.js
@@ -3,5 +3,6 @@ var searchData=
   ['_5fappenders',['_appenders',['../class_cpp_logging_1_1_processor.html#a1be332fe9c9fc637970fd533f2280a89',1,'CppLogging::Processor']]],
   ['_5ffilters',['_filters',['../class_cpp_logging_1_1_processor.html#aaef53974adcf8fdc7934d4b739082185',1,'CppLogging::Processor']]],
   ['_5flayout',['_layout',['../class_cpp_logging_1_1_processor.html#a49700ff578bb14ee285c9d8cea442358',1,'CppLogging::Processor']]],
-  ['_5fprocessors',['_processors',['../class_cpp_logging_1_1_processor.html#ab52ee1a818777d9d9d725c74280f102f',1,'CppLogging::Processor']]]
+  ['_5fprocessors',['_processors',['../class_cpp_logging_1_1_processor.html#ab52ee1a818777d9d9d725c74280f102f',1,'CppLogging::Processor']]],
+  ['_5fstarted',['_started',['../class_cpp_logging_1_1_processor.html#a911e46ccd6dc5664e81ff16bd6d23df8',1,'CppLogging::Processor']]]
 ];
