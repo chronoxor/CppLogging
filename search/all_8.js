35c8ce8730fc7dd3bc8786e95de76d906c8a22bd
Travis CI <travis@travis-ci.org>
2019-05-23 08:54:48 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a68863a49a2fb24e5a5760bf44eef387f',1,'CppLogging::Logger::Info()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
+  ['info',['Info',['../class_cpp_logging_1_1_logger.html#a4b893bb6ec96c212ee2fade51a206cc5',1,'CppLogging::Logger::Info()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging::INFO()']]],
   ['isenabled',['IsEnabled',['../class_cpp_logging_1_1_trigger.html#a3ec7ca4993b7ee110fccc5984caa7901',1,'CppLogging::Trigger']]],
   ['isformatstored',['IsFormatStored',['../class_cpp_logging_1_1_record.html#ac698292f7a70db2effa97351eadc21b6',1,'CppLogging::Record']]],
-  ['isstarted',['IsStarted',['../class_cpp_logging_1_1_file_appender.html#a4bb21dfc20432ee19c36a291fc566ff4',1,'CppLogging::FileAppender::IsStarted()'],['../class_cpp_logging_1_1_rolling_file_appender.html#abf7ad312671b029e74df617c1993fe65',1,'CppLogging::RollingFileAppender::IsStarted()'],['../class_cpp_logging_1_1_element.html#af6d8906d2a726c1e3702761d1358d243',1,'CppLogging::Element::IsStarted()'],['../class_cpp_logging_1_1_processor.html#acb403093781f7f3a61570dedf28f55ad',1,'CppLogging::Processor::IsStarted()']]]
+  ['isstarted',['IsStarted',['../class_cpp_logging_1_1_file_appender.html#a1859345300810c84ad9c2d89eb9695f5',1,'CppLogging::FileAppender::IsStarted()'],['../class_cpp_logging_1_1_rolling_file_appender.html#a65a72e0a6b1da819952b282f496cc5e6',1,'CppLogging::RollingFileAppender::IsStarted()'],['../class_cpp_logging_1_1_element.html#af6d8906d2a726c1e3702761d1358d243',1,'CppLogging::Element::IsStarted()'],['../class_cpp_logging_1_1_processor.html#a4ad7872e518e6e6386db31ff3224ff5e',1,'CppLogging::Processor::IsStarted()']]]
 ];
