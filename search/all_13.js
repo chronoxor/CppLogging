b8aa12685ae021b05a5f7a3236f7caaa0c29b6d8
Travis CI <travis@travis-ci.org>
2019-07-10 14:43:30 +0000

    Update generated documentation

--- search/all_13.js
+++ search/all_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['warn',['Warn',['../class_cpp_logging_1_1_logger.html#a9efe3d8c776fcd8fa2013ee1e2a06bdb',1,'CppLogging::Logger::Warn()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba32bd8a1db2275458673903bdb84cb277',1,'CppLogging::WARN()']]]
+  ['warn',['Warn',['../class_cpp_logging_1_1_logger.html#a7d546dda5077b66548ec5951a297d017',1,'CppLogging::Logger::Warn()'],['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba32bd8a1db2275458673903bdb84cb277',1,'CppLogging::WARN()']]]
 ];
