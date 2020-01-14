43cec9ef834d9bb14c5e63e71f80c483e2e08b64
Travis CI <travis@travis-ci.org>
2020-01-14 15:02:26 +0000

    Update generated documentation

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['warn',['Warn',['../class_cpp_logging_1_1_logger.html#a7d546dda5077b66548ec5951a297d017',1,'CppLogging::Logger']]]
+  ['warn_401',['Warn',['../class_cpp_logging_1_1_logger.html#a7d546dda5077b66548ec5951a297d017',1,'CppLogging::Logger']]]
 ];
