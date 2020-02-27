07a6d1229549f88ed22f2db266b082287b5dd801
Travis CI <travis@travis-ci.org>
2020-02-27 23:45:14 +0000

    Update generated documentation

--- search/enumvalues_6.js
+++ search/enumvalues_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['minute_472',['MINUTE',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a46bda4cde2f10bdb9e51e3bbefa4a2bf',1,'CppLogging']]],
-  ['month_473',['MONTH',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a383316922f2516abe5abe53370284a7f',1,'CppLogging']]]
+  ['minute',['MINUTE',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a46bda4cde2f10bdb9e51e3bbefa4a2bf',1,'CppLogging']]],
+  ['month',['MONTH',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a383316922f2516abe5abe53370284a7f',1,'CppLogging']]]
 ];
