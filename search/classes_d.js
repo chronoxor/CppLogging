533450932e7e71b183b0df5f7a7178f6bdb7ad6a
Travis CI <travis@travis-ci.org>
2020-02-28 00:14:36 +0000

    Update generated documentation

--- search/classes_d.js
+++ search/classes_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['textlayout',['TextLayout',['../class_cpp_logging_1_1_text_layout.html',1,'CppLogging']]],
-  ['trigger',['Trigger',['../class_cpp_logging_1_1_trigger.html',1,'CppLogging']]]
+  ['textlayout_253',['TextLayout',['../class_cpp_logging_1_1_text_layout.html',1,'CppLogging']]],
+  ['trigger_254',['Trigger',['../class_cpp_logging_1_1_trigger.html',1,'CppLogging']]]
 ];
--- search/enumvalues_5.js
+++ search/enumvalues_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['info',['INFO',['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging']]]
+  ['info_471',['INFO',['../namespace_cpp_logging.html#ac4e889a814b82af010ef93be2e618f3ba551b723eafd6a31d444fcb2f5920fbd3',1,'CppLogging']]]
 ];
