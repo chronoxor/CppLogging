4583467f346ddcc8d77c69006f174ca0e993702f
Travis CI <travis@travis-ci.org>
2020-02-28 00:38:15 +0000

    Update generated documentation

--- search/classes_4.js
+++ search/classes_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['element',['Element',['../class_cpp_logging_1_1_element.html',1,'CppLogging']]],
-  ['emptylayout',['EmptyLayout',['../class_cpp_logging_1_1_empty_layout.html',1,'CppLogging']]],
-  ['errorappender',['ErrorAppender',['../class_cpp_logging_1_1_error_appender.html',1,'CppLogging']]],
-  ['exclusiveprocessor',['ExclusiveProcessor',['../class_cpp_logging_1_1_exclusive_processor.html',1,'CppLogging']]]
+  ['element_232',['Element',['../class_cpp_logging_1_1_element.html',1,'CppLogging']]],
+  ['emptylayout_233',['EmptyLayout',['../class_cpp_logging_1_1_empty_layout.html',1,'CppLogging']]],
+  ['errorappender_234',['ErrorAppender',['../class_cpp_logging_1_1_error_appender.html',1,'CppLogging']]],
+  ['exclusiveprocessor_235',['ExclusiveProcessor',['../class_cpp_logging_1_1_exclusive_processor.html',1,'CppLogging']]]
 ];
