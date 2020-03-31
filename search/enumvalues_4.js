6f78c6461b4035bd524a1d1119258743411a51a8
Travis CI <travis@travis-ci.org>
2020-03-31 17:10:43 +0000

    Update generated documentation

--- search/enumvalues_4.js
+++ search/enumvalues_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hour',['HOUR',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57add3f965174e8bb2f64523981626ced1a',1,'CppLogging']]]
+  ['hour_470',['HOUR',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57add3f965174e8bb2f64523981626ced1a',1,'CppLogging']]]
 ];
