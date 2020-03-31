d696bdf1fe956a845f4950e6af831c1a17307c9e
Travis CI <travis@travis-ci.org>
2020-03-31 16:39:19 +0000

    Update generated documentation

--- search/all_11.js
+++ search/all_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['update',['Update',['../class_cpp_logging_1_1_level_filter.html#ae0c18280fc0c4b3b910e4c91dddab5b2',1,'CppLogging::LevelFilter::Update(Level level, bool positive=true)'],['../class_cpp_logging_1_1_level_filter.html#a613e6c1921b87b41ef82a4b8aeba9942',1,'CppLogging::LevelFilter::Update(Level from, Level to, bool positive=true)'],['../class_cpp_logging_1_1_switch_filter.html#a212f19c0bc775a6b877c026b2cd5728c',1,'CppLogging::SwitchFilter::Update()'],['../class_cpp_logging_1_1_logger.html#ae0513995cc4acc8757837219cbcec616',1,'CppLogging::Logger::Update()']]]
+  ['update_189',['Update',['../class_cpp_logging_1_1_level_filter.html#ae0c18280fc0c4b3b910e4c91dddab5b2',1,'CppLogging::LevelFilter::Update(Level level, bool positive=true)'],['../class_cpp_logging_1_1_level_filter.html#a613e6c1921b87b41ef82a4b8aeba9942',1,'CppLogging::LevelFilter::Update(Level from, Level to, bool positive=true)'],['../class_cpp_logging_1_1_switch_filter.html#a212f19c0bc775a6b877c026b2cd5728c',1,'CppLogging::SwitchFilter::Update()'],['../class_cpp_logging_1_1_logger.html#ae0513995cc4acc8757837219cbcec616',1,'CppLogging::Logger::Update()']]]
 ];
