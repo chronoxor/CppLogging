cc2fd3bdf56b7b2c40e967bf8750728573f7b468
Travis CI <travis@travis-ci.org>
2020-05-22 22:41:30 +0000

    Update generated documentation

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['nullappender_244',['NullAppender',['../class_cpp_logging_1_1_null_appender.html',1,'CppLogging']]],
-  ['nulllayout_245',['NullLayout',['../class_cpp_logging_1_1_null_layout.html',1,'CppLogging']]]
+  ['nullappender',['NullAppender',['../class_cpp_logging_1_1_null_appender.html',1,'CppLogging']]],
+  ['nulllayout',['NullLayout',['../class_cpp_logging_1_1_null_layout.html',1,'CppLogging']]]
 ];
--- search/files_c.js
+++ search/files_c.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['switch_5ffilter_2ecpp_310',['switch_filter.cpp',['../switch__filter_8cpp.html',1,'']]],
-  ['switch_5ffilter_2eh_311',['switch_filter.h',['../switch__filter_8h.html',1,'']]],
-  ['sync_5fprocessor_2ecpp_312',['sync_processor.cpp',['../sync__processor_8cpp.html',1,'']]],
-  ['sync_5fprocessor_2eh_313',['sync_processor.h',['../sync__processor_8h.html',1,'']]],
-  ['syslog_5fappender_2ecpp_314',['syslog_appender.cpp',['../syslog__appender_8cpp.html',1,'']]],
-  ['syslog_5fappender_2eh_315',['syslog_appender.h',['../syslog__appender_8h.html',1,'']]]
+  ['switch_5ffilter_2ecpp',['switch_filter.cpp',['../switch__filter_8cpp.html',1,'']]],
+  ['switch_5ffilter_2eh',['switch_filter.h',['../switch__filter_8h.html',1,'']]],
+  ['sync_5fprocessor_2ecpp',['sync_processor.cpp',['../sync__processor_8cpp.html',1,'']]],
+  ['sync_5fprocessor_2eh',['sync_processor.h',['../sync__processor_8h.html',1,'']]],
+  ['syslog_5fappender_2ecpp',['syslog_appender.cpp',['../syslog__appender_8cpp.html',1,'']]],
+  ['syslog_5fappender_2eh',['syslog_appender.h',['../syslog__appender_8h.html',1,'']]]
 ];
