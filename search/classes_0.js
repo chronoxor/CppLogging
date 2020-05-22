a72186bf0c5c7eae04cf14e557bda1d1733f6f1c
Travis CI <travis@travis-ci.org>
2020-05-22 21:23:50 +0000

    Update generated documentation

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['appender_222',['Appender',['../class_cpp_logging_1_1_appender.html',1,'CppLogging']]],
-  ['asyncwaitfreeprocessor_223',['AsyncWaitFreeProcessor',['../class_cpp_logging_1_1_async_wait_free_processor.html',1,'CppLogging']]],
-  ['asyncwaitfreequeue_224',['AsyncWaitFreeQueue',['../class_cpp_logging_1_1_async_wait_free_queue.html',1,'CppLogging']]],
-  ['asyncwaitfreequeue_3c_20cpplogging_3a_3arecord_20_3e_225',['AsyncWaitFreeQueue&lt; CppLogging::Record &gt;',['../class_cpp_logging_1_1_async_wait_free_queue.html',1,'CppLogging']]],
-  ['asyncwaitprocessor_226',['AsyncWaitProcessor',['../class_cpp_logging_1_1_async_wait_processor.html',1,'CppLogging']]]
+  ['appender',['Appender',['../class_cpp_logging_1_1_appender.html',1,'CppLogging']]],
+  ['asyncwaitfreeprocessor',['AsyncWaitFreeProcessor',['../class_cpp_logging_1_1_async_wait_free_processor.html',1,'CppLogging']]],
+  ['asyncwaitfreequeue',['AsyncWaitFreeQueue',['../class_cpp_logging_1_1_async_wait_free_queue.html',1,'CppLogging']]],
+  ['asyncwaitfreequeue_3c_20cpplogging_3a_3arecord_20_3e',['AsyncWaitFreeQueue&lt; CppLogging::Record &gt;',['../class_cpp_logging_1_1_async_wait_free_queue.html',1,'CppLogging']]],
+  ['asyncwaitprocessor',['AsyncWaitProcessor',['../class_cpp_logging_1_1_async_wait_processor.html',1,'CppLogging']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['layout_238',['Layout',['../class_cpp_logging_1_1_layout.html',1,'CppLogging']]],
-  ['levelfilter_239',['LevelFilter',['../class_cpp_logging_1_1_level_filter.html',1,'CppLogging']]],
-  ['logger_240',['Logger',['../class_cpp_logging_1_1_logger.html',1,'CppLogging']]],
-  ['loggerfilter_241',['LoggerFilter',['../class_cpp_logging_1_1_logger_filter.html',1,'CppLogging']]]
+  ['layout',['Layout',['../class_cpp_logging_1_1_layout.html',1,'CppLogging']]],
+  ['levelfilter',['LevelFilter',['../class_cpp_logging_1_1_level_filter.html',1,'CppLogging']]],
+  ['logger',['Logger',['../class_cpp_logging_1_1_logger.html',1,'CppLogging']]],
+  ['loggerfilter',['LoggerFilter',['../class_cpp_logging_1_1_logger_filter.html',1,'CppLogging']]]
 ];
