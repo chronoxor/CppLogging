53ddeccf46fdbb25f7825049479452105c581441
Travis CI <travis@travis-ci.org>
2020-05-22 22:42:57 +0000

    Update generated documentation

--- search/files_9.js
+++ search/files_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ostream_5fappender_2ecpp',['ostream_appender.cpp',['../ostream__appender_8cpp.html',1,'']]],
-  ['ostream_5fappender_2eh',['ostream_appender.h',['../ostream__appender_8h.html',1,'']]]
+  ['ostream_5fappender_2ecpp_300',['ostream_appender.cpp',['../ostream__appender_8cpp.html',1,'']]],
+  ['ostream_5fappender_2eh_301',['ostream_appender.h',['../ostream__appender_8h.html',1,'']]]
 ];
