e9e1afa42de41ead1e96a854aef3ee1df180006e
Travis CI <travis@travis-ci.org>
2020-05-22 21:02:23 +0000

    Update generated documentation

--- search/enums_2.js
+++ search/enums_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['timerollingpolicy',['TimeRollingPolicy',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57',1,'CppLogging']]]
+  ['timerollingpolicy_445',['TimeRollingPolicy',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57',1,'CppLogging']]]
 ];
