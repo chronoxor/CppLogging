04caef18c97936eea8e7f2559792c99ee4d85979
Travis CI <travis@travis-ci.org>
2020-05-22 19:09:58 +0000

    Update generated documentation

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['level_436',['level',['../class_cpp_logging_1_1_record.html#a4f673dff61bc47ca855b489cdc8ac227',1,'CppLogging::Record']]],
-  ['logger_437',['logger',['../class_cpp_logging_1_1_record.html#a8791d7c6170eda83edc33e66a5d1a9eb',1,'CppLogging::Record']]]
+  ['level',['level',['../class_cpp_logging_1_1_record.html#a4f673dff61bc47ca855b489cdc8ac227',1,'CppLogging::Record']]],
+  ['logger',['logger',['../class_cpp_logging_1_1_record.html#a8791d7c6170eda83edc33e66a5d1a9eb',1,'CppLogging::Record']]]
 ];
