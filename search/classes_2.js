ea75f2369d5d9af6c0ce77adf67aa7354f92547e
Travis CI <travis@travis-ci.org>
2020-05-22 18:27:24 +0000

    Update generated documentation

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['config_229',['Config',['../class_cpp_logging_1_1_config.html',1,'CppLogging']]],
-  ['consoleappender_230',['ConsoleAppender',['../class_cpp_logging_1_1_console_appender.html',1,'CppLogging']]]
+  ['config',['Config',['../class_cpp_logging_1_1_config.html',1,'CppLogging']]],
+  ['consoleappender',['ConsoleAppender',['../class_cpp_logging_1_1_console_appender.html',1,'CppLogging']]]
 ];
