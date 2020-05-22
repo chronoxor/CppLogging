18c5a60875a926fbc354039faabf8f308f5430b0
Travis CI <travis@travis-ci.org>
2020-05-22 21:15:42 +0000

    Update generated documentation

--- search/files_a.js
+++ search/files_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['processor_2ecpp_302',['processor.cpp',['../processor_8cpp.html',1,'']]],
-  ['processor_2eh_303',['processor.h',['../processor_8h.html',1,'']]],
-  ['processors_2eh_304',['processors.h',['../processors_8h.html',1,'']]]
+  ['processor_2ecpp',['processor.cpp',['../processor_8cpp.html',1,'']]],
+  ['processor_2eh',['processor.h',['../processor_8h.html',1,'']]],
+  ['processors_2eh',['processors.h',['../processors_8h.html',1,'']]]
 ];
--- search/files_d.js
+++ search/files_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['text_5flayout_2ecpp_316',['text_layout.cpp',['../text__layout_8cpp.html',1,'']]],
-  ['text_5flayout_2eh_317',['text_layout.h',['../text__layout_8h.html',1,'']]],
-  ['trigger_2eh_318',['trigger.h',['../trigger_8h.html',1,'']]]
+  ['text_5flayout_2ecpp',['text_layout.cpp',['../text__layout_8cpp.html',1,'']]],
+  ['text_5flayout_2eh',['text_layout.h',['../text__layout_8h.html',1,'']]],
+  ['trigger_2eh',['trigger.h',['../trigger_8h.html',1,'']]]
 ];
