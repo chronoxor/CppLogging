5a50a44ac6b151d78aa49c4480c0d3a2c7a13fce
Travis CI <travis@travis-ci.org>
2020-08-21 20:47:51 +0000

    Update generated documentation

--- search/enumvalues_8.js
+++ search/enumvalues_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['second_475',['SECOND',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a2200becb80f0019c4a2ccecec350d0db',1,'CppLogging']]]
+  ['second',['SECOND',['../namespace_cpp_logging.html#a2ae7ad16f5b402d86a07604253464f57a2200becb80f0019c4a2ccecec350d0db',1,'CppLogging']]]
 ];
