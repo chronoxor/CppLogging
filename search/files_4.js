6e53c96b211a6b5158062ca8fc438c7cfff83b56
Travis CI <travis@travis-ci.org>
2020-08-25 19:04:45 +0000

    Update generated documentation

--- search/files_4.js
+++ search/files_4.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['element_2eh',['element.h',['../element_8h.html',1,'']]],
-  ['empty_5flayout_2eh',['empty_layout.h',['../empty__layout_8h.html',1,'']]],
-  ['error_5fappender_2ecpp',['error_appender.cpp',['../error__appender_8cpp.html',1,'']]],
-  ['error_5fappender_2eh',['error_appender.h',['../error__appender_8h.html',1,'']]],
-  ['exclusive_5fprocessor_2ecpp',['exclusive_processor.cpp',['../exclusive__processor_8cpp.html',1,'']]],
-  ['exclusive_5fprocessor_2eh',['exclusive_processor.h',['../exclusive__processor_8h.html',1,'']]]
+  ['element_2eh_274',['element.h',['../element_8h.html',1,'']]],
+  ['empty_5flayout_2eh_275',['empty_layout.h',['../empty__layout_8h.html',1,'']]],
+  ['error_5fappender_2ecpp_276',['error_appender.cpp',['../error__appender_8cpp.html',1,'']]],
+  ['error_5fappender_2eh_277',['error_appender.h',['../error__appender_8h.html',1,'']]],
+  ['exclusive_5fprocessor_2ecpp_278',['exclusive_processor.cpp',['../exclusive__processor_8cpp.html',1,'']]],
+  ['exclusive_5fprocessor_2eh_279',['exclusive_processor.h',['../exclusive__processor_8h.html',1,'']]]
 ];
--- search/related_2.js
+++ search/related_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['timepolicyimpl',['TimePolicyImpl',['../class_cpp_logging_1_1_rolling_file_appender.html#a6d81316f3ffde8a367df1396d02a7d6f',1,'CppLogging::RollingFileAppender']]]
+  ['timepolicyimpl_481',['TimePolicyImpl',['../class_cpp_logging_1_1_rolling_file_appender.html#a6d81316f3ffde8a367df1396d02a7d6f',1,'CppLogging::RollingFileAppender']]]
 ];
