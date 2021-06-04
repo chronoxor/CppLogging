dc27ea4a3cdfb1766f6619a868be4ab8aabfe9bf
Ivan Shynkarenka <chronoxor@gmail.com>
2021-06-04 16:55:51 +0300

    Version 1.0.1.0

--- include/logging/version.h
+++ include/logging/version.h
@@ -31,7 +31,7 @@ GitHub: https://github.com/chronoxor/CppLogging
 namespace CppLogging {
 
 //! Project version
-const char version[] = "1.0.0.0";
+const char version[] = "1.0.1.0";
 
 } // namespace CppLogging
 
