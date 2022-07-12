215d430133d5f15eb2a822e50c2c8b9dcc5c344a
chronoxor <chronoxor@users.noreply.github.com>
2022-07-12 22:21:31 +0000

    Update generated documentation

--- search/functions_11.js
+++ search/functions_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['warn_411',['Warn',['../class_cpp_logging_1_1_logger.html#ab66d1e6b9e2a825658801b3a63e37caa',1,'CppLogging::Logger::Warn(std::string_view message)'],['../class_cpp_logging_1_1_logger.html#aceb70f1ef7e6dd4b3d9d3a19e6a9a78f',1,'CppLogging::Logger::Warn(fmt::format_string&lt; T... &gt; message, T &amp;&amp;... args)']]]
+  ['warn_411',['Warn',['../class_cpp_logging_1_1_logger.html#a4c8e374f04b06c7bf2fdd88ccc4a60c0',1,'CppLogging::Logger::Warn(std::string_view message) const'],['../class_cpp_logging_1_1_logger.html#a2eec3a554f5ae9755d942976887f4cff',1,'CppLogging::Logger::Warn(fmt::format_string&lt; T... &gt; message, T &amp;&amp;... args) const']]]
 ];
