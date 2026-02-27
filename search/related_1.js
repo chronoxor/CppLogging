6c99bbd6071acc25133966726a59a23abd7dd237
chronoxor <chronoxor@users.noreply.github.com>
2026-02-27 14:12:56 +0000

    Update generated documentation

--- search/related_1.js
+++ search/related_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['sizepolicyimpl_0',['SizePolicyImpl',['../class_cpp_logging_1_1_rolling_file_appender.html#a0d453fa68a659898193acb608bf94cda',1,'CppLogging::RollingFileAppender']]],
-  ['swap_1',['swap',['../class_cpp_logging_1_1_record.html#a41307f7cef7933943d41aa2f13fcef01',1,'CppLogging::Record']]]
+  ['swap_1',['swap',['../class_cpp_logging_1_1_record.html#aa2a69386b66d5d31c080c480b3e73712',1,'CppLogging::Record']]]
 ];
